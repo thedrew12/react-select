9ef9986292824ec2679ffa2fd0bb78fdfc7d5935
Andrew Sumpter <andrew.sumpter@economicmodeling.com>
2017-01-23 14:07:45 -0800

    testing something

--- src/Select.js
+++ src/Select.js
@@ -1104,7 +1104,7 @@ const Select = React.createClass({
 					{this.renderClear()}
 					{this.renderArrow()}
 				</div>
-				{isOpen ? this.renderOuter(options, !this.props.multi ? valueArray : null, focusedOption) : null}
+				{this.renderOuter(options, !this.props.multi ? valueArray : null, focusedOption)}
 			</div>
 		);
 	}
